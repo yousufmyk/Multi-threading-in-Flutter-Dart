b39bcc6c79a6732c4652981b859ed6cb4de08629
yousufmyk <mohamedyousufdev@gmail.com>
2023-09-30 23:23:49 +0500

    First method of multiThreading

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,40 @@
+import 'dart:convert';
+import 'dart:io';
+import 'dart:isolate';
+
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(const MyApp());
+}
+
+class Person{
+  final String name;
+  final int age;
+
+  Person({required this.name, required this.age});
+
+  Person.frromJson(Map<String, dynamic> json) 
+  
+  : name = json['name'] as String,
+  age = json['age'] as int; 
+}
+
+Future<I>
+
+void _getPersons (SendPort sp) async {
+  const url = 'http://127.0.0.1:5500/apis/people1.json';
+  final persons = await HttpClient()
+  .getUrl(Uri.parse(url))
+  .then((req) => req.close())
+  .then((response) => response.transform(utf8.decoder).join())
+  .then((jsonString) => json.decode(jsonString) as List<dynamic>)
+  .then((json) => json.map((map) => Person.frromJson(map)));
+
+  Isolate.exit(
+    sp,
+    persons,
+  );
 }
 
 class MyApp extends StatelessWidget {
